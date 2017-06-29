3e515f03d80be1a3f3359ffa7c1955d189b9f2a6
michel perez <michel.ingesoft@gmail.com>
2017-06-29 17:09:55 -0500

    wip

--- lib/funct.ml
+++ lib/funct.ml
@@ -1,16 +1,64 @@
-module type X_int = sig
-  val x: int
+open Core.Std
+
+module type Summer = sig
+  type t
+  val sum : t -> t -> t
 end
 
-module Increment (M : X_int) : X_int = struct
-  let x = M.x + 1
+module type Summable = sig
+  type t
+  val zero : t
+  val unit : t
 end
 
-module Zero = struct
-  let x = 0
+module type Incrementor = sig
+  type t
+  val inc : t -> t
 end
 
-module One = Increment(Zero)
+module IntSummable = struct
+  type t = int
+  let zero = 0
+  let unit = 1
+end
+
+module FloatSummable = struct
+  type t = float
+  let zero = 0.
+  let unit = 1.
+end
+
+module IntSummer = struct
+  type t = int
+  let sum n1 n2 =
+    n1 + n2
+end
+
+module FloatSummer = struct
+  type t = float
+  let sum n1 n2 =
+    n1 +. n2
+end
+
+module Make_incrementor (A : Summable) (M : Summer with type t = A.t) : (Incrementor with type t = A.t) = struct
+  include M
+  let inc n = M.sum n A.unit
+end
+
+module Int_incrementor = Make_incrementor (IntSummable) (IntSummer)
+
+module Float_incrementor = Make_incrementor (FloatSummable) (FloatSummer)
+
+let increment (type a) ((module A) : (module Incrementor with type t = a)) (n : a) =
+  A.inc n
+
+let apply_twice f n =
+  f (f n)
+
+let increment_twice (type a) ((module A) : (module Incrementor with type t = a)) (n : a) =
+  apply_twice (increment (module A)) n
 
 let () =
-  print_int (One.x)
+  printf "int %d\n" (increment (module Int_incrementor) 10);
+  printf "float %f\n" (increment (module Float_incrementor) 10.);
+  printf "float twice %f\n" (increment_twice (module Float_incrementor) 10.);

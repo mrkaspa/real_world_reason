88fba6f22c16d9a786423ad9adad8cbdf3d67488
michel perez <michel.ingesoft@gmail.com>
2017-08-14 17:00:47 -0500

    wip

--- lib/run.ml
+++ lib/run.ml
@@ -6,5 +6,10 @@ let rec read_and_accumulate accum =
   | None -> accum
   | Some x -> read_and_accumulate (accum +. Float.of_string x)
 
+let is_empty =
+  function
+  | [] -> true
+  | _ -> false
+
 let run () =
   printf "Total: %F\n" (read_and_accumulate 0.)
